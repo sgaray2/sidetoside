4ded934a7932b9a072ec889c37d2df75995c3403
sabrina <sabrigaray13@gmail.com>
2020-05-02 19:04:35 -0700

    finish header and carousel details

--- app.js
+++ app.js
@@ -8,7 +8,37 @@ app.get("/", function(req,res){
   res.sendFile(__dirname + "/homepage.html");
 });
 
+app.get("/homepage.html", function(req,res){
+    res.sendFile(__dirname + "/homepage.html");
+});
 
-app.listen(process.env.PORT, function(){
+app.get("/homepage.html", function(req,res){
+    res.sendFile(__dirname + "/homepage.html");
+});
+
+app.get("/about-us.html", function(req,res){
+    res.sendFile(__dirname + "/about-us.html");
+});
+
+app.get("/Shop.html", function(req,res){
+    res.sendFile(__dirname + "/Shop.html");
+});
+
+app.get("/Contact-us.html", function(req,res){
+    res.sendFile(__dirname + "/Contact-us.html");
+});
+
+app.get("/Shopping-bag.html", function(req,res){
+    res.sendFile(__dirname + "/Shopping-bag.html");
+});
+
+app.get("/user-account.html", function(req,res){
+    res.sendFile(__dirname + "/user-account.html");
+});
+
+
+
+
+app.listen(3000, function(){
   console.log("ready");
 });
